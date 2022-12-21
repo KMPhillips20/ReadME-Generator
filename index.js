267c7296c4c8b3a798219d6fd6a264d80ca5197d
Kyle <kylemphillips65@yahoo.com>
2022-12-20 21:26:53 -0500

    added the sections that the readme would have

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 // TODO: Create an array of questions for user input
 const questions = require("questions");
 const links = require("links");
+const fs = require("fs");
 
 questions
 .prompt([
@@ -60,7 +61,54 @@ var description = response.description;
 var usage = response.usage;
 var license = response.license;
 var contributions = reponse.contributions;
-var test = response.test;
-var badgeLabel = response.badgeLabel;
+var tests = response.tests;
+var badgeLabel =
+var badgeUrl =
+
+
+
+
+
+ #${title}
+***
+## Description
+${description}
+***
+## Table of Contents
+- Installation
+- Usage
+- Licensing Info
+- Contributions
+- Tests
+- Creator Info
+***
+## Installation Requirements
+${install}
+***
+## Usage
+${usage}
+***
+## Licensing Info
+${license}
+***
+## Contributions
+${contributions}
+***
+## Tests
+${tests}
+***
+
+        
+
+
+
+
+  
 
 module.exports = generateMarkdown;
+
+
+
+
+
+// [![License: ${encodeURIComponent(data.license)}](https://img.shields.io/badge/License-${encodeURIComponent(data.license)}-yellow.svg)](https://opensource.org/licenses/${encodeURIComponent(data.license)})
