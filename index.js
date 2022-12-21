5efa12b27dc59a99c79470712f45f195fa7a17d0
Kyle <kylemphillips65@yahoo.com>
2022-12-20 20:23:38 -0500

    got my questions filled out with the right information that needs to be in the readme

--- index.js
+++ index.js
@@ -1,7 +1,55 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ([
+    {
+    type: "input",
+    message: "What is your Github username?",
+    name: "username"
+  },
+  {
+    type: "input",
+    message: "What is the title of your project?",
+    name: "title"
+  },
+  {
+    type: "input",
+    message: "Give a description of your project:",
+    name: "description"
+  },
+  {
+    type: "input",
+    message: "Give the installation process here:",
+    name: "install"
+  },
+  {
+    type: "input",
+    message: "Outline usage information for your project:",
+    name: "usage"
+  },
+  {
+    type: "input",
+    message: "Explain licensing of your project:",
+    name: "license"
+  },
+  {
+    type: "input",
+    message: "What contributions will be considered for this project?",
+    name: "contributions"
+  },
+  {
+    type: "input",
+    message: "What tests does this project feature if there are any?",
+    name: "tests"
+  },
+  {
+    type: "input",
+    message: "What badge label would you like to use for your project?",
+    name: "badgeLabel"
+  },
+  
+])
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
