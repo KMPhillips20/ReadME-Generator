eb47f00816960c228270b257c111a2e9478d789a
Kyle <kylemphillips65@yahoo.com>
2022-12-20 20:35:58 -0500

    added two const for my links and questions

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = ([
+const questions = require("questions");
+const links = require("links");
+
+questions
+.prompt([
     {
     type: "input",
     message: "What is your Github username?",
@@ -51,11 +55,4 @@ const questions = ([
 ])
 
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
